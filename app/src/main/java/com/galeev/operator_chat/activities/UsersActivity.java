bd762564fb51d7b1d6734f4dc3879533966ec15a
Danil <danilhafizov123kk@gmail.com>
2024-02-29 21:18:03 +0500

    Добавлена логика отображения пользователей: Если пользователь не администратор, то на экране он увидит только аккаунт администратора; Если пользователь администратор, то на экране он увидит полный список пользователей кто не является администратором

--- app/src/main/java/com/galeev/operator_chat/activities/UsersActivity.java
+++ app/src/main/java/com/galeev/operator_chat/activities/UsersActivity.java
@@ -3,6 +3,7 @@ package com.galeev.operator_chat.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -20,7 +21,6 @@ public class UsersActivity extends BaseActivity implements UserListener {
 
     private ActivityUsersBinding binding;
     private PreferenceManager preferenceManager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +33,7 @@ public class UsersActivity extends BaseActivity implements UserListener {
 private void setListeners(){
         binding.imageBack.setOnClickListener(v -> onBackPressed());
 }
-
-    private void getUsers(){
+    private void getUsers() {
         loading(true);
         FirebaseFirestore database = FirebaseFirestore.getInstance();
         database.collection(Constants.KEY_COLLECTION_USERS)
@@ -42,10 +41,13 @@ private void setListeners(){
                 .addOnCompleteListener(task -> {
                     loading(false);
                     String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
-                    if(task.isSuccessful() && task.getResult() != null) {
+                    String currentUserRole = preferenceManager.getString(Constants.KEY_ROLE); // Получаем роль текущего пользователя
+                    if (task.isSuccessful() && task.getResult() != null) {
                         List<User> users = new ArrayList<>();
                         for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
-                            if (currentUserId.equals(queryDocumentSnapshot.getId())) {
+                            String userId = queryDocumentSnapshot.getId();
+                            String userRole = queryDocumentSnapshot.getString(Constants.KEY_ROLE);
+                            if (currentUserId.equals(userId)) {
                                 continue;
                             }
                             User user = new User();
@@ -54,9 +56,21 @@ private void setListeners(){
                             user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
                             user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
                             user.id = queryDocumentSnapshot.getId();
-                            users.add(user);
+                            // Проверяем роль текущего пользователя и добавляем пользователя в список
+                            if ("Администратор".equals(currentUserRole)) {
+                                // Если текущий пользователь администратор, отображаем всех пользователей, кроме текущего
+                                users.add(user);
+                            } else if ("Администратор".equals(userRole)) {
+                                // Если текущий пользователь не администратор, отображаем только аккаунт администратора
+                                users.clear(); // Очищаем список от других пользователей
+                                users.add(user);
+                                break; // Прерываем цикл после добавления аккаунта администратора
+                            } else {
+                                // Если ни текущий пользователь, ни текущий пользователь из списка не администраторы, отображаем всех пользователей
+                                users.add(user);
+                            }
                         }
-                        if(users.size() > 0){
+                        if (users.size() > 0) {
                             UsersAdapter usersAdapter = new UsersAdapter(users, this);
                             binding.usersRecyclerView.setAdapter(usersAdapter);
                             binding.usersRecyclerView.setVisibility(View.VISIBLE);
@@ -68,12 +82,10 @@ private void setListeners(){
                     }
                 });
     }
-
     private void showErrorMessage(){
         binding.textErrorMessage.setText(String.format("%s", "Нет доступного пользователя"));
         binding.textErrorMessage.setVisibility(View.VISIBLE);
     }
-
     private void loading(Boolean isLoading){
         if(isLoading){
             binding.progressBar.setVisibility(View.VISIBLE);
@@ -82,6 +94,9 @@ private void setListeners(){
         }
     }
 
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
     @Override
     public void onUserClicked(User user) {
         Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
